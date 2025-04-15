60c979b2c0fcccd60b9011f12d51e6f11c1cde5b
MdMehediHasan22 <mdmehedi3799@gmail.com>
2025-04-15 23:38:57 +0600

    service banner added

--- src/app/services/[id]/page.jsx
+++ src/app/services/[id]/page.jsx
@@ -1,5 +1,6 @@
  import dbConnect, { collectionNamesObj } from "@/lib/dbConnect";
 import { ObjectId } from "mongodb";
+import Image from "next/image";
 import React from "react";
 
  export default async function ServiceDetailsPage({params}){
@@ -8,6 +9,15 @@ import React from "react";
     const data = await servicesCollection.findOne({_id: new ObjectId(p.id)});
     return (
         <div>
+            <section>
+                <figure className="flex justify-center w-full">
+                <Image src={"/assets/images/checkout/checkout.png"}
+                width={1137}
+                height={300}
+                alt={"banner"}
+                />
+                </figure>
+            </section>
             <p>{p.id}</p>
             <p>{JSON.stringify(data)}</p>
         </div>
